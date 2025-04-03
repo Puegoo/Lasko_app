ac5637e789b83e7145bb92d872f8c08f4ce6ab4f
Puegoo <nowak.kpiotr@icloud.com>
2025-04-03 14:42:05 +0200

    Add initial files for backend and frontend

---
+++ backend/lasko_backend/celery.py
@@ -0,0 +1,7 @@
+import os
+from celery import Celery
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'lasko_backend.settings')
+app = Celery('lasko_backend')
+app.config_from_object('django.conf:settings', namespace='CELERY')
+app.autodiscover_tasks()
